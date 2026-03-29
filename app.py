825a67e7a225d93e567b2978af85788fe1384798
Younus <younusmd7587@gmail.com>
2026-03-29 18:11:03 +0530

    Initial commit - car auction app

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from db_config import get_db_connection
+
+app = Flask(__name__)
+app.secret_key = "secret_key"
+
+# Home -> Login
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+# Register Page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = generate_password_hash(request.form['password'])
+        phone = request.form['phone']
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO users (name, email, password, phone) VALUES (%s, %s, %s, %s)",
+            (name, email, password, phone)
+        )
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        return redirect('/')
+
+    return render_template('register.html')
+
+# Login
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM users WHERE email=%s", (email,))
+    user = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    if user and check_password_hash(user['password'], password):
+        session['user'] = user['name']
+        return redirect('/dashboard')
+
+    return "Invalid credentials ❌"
+
+# Dashboard
+@app.route('/dashboard')
+def dashboard():
+    if 'user' in session:
+        return render_template('dashboard.html', user=session['user'])
+    return redirect('/')
+
+# Logout
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
